36031e567db587ef2731dbe908e4a35c9263e9a6
Michael Pimentel Jr <mtpjr88@gmail.com>
2018-12-22 06:07:29 -0800

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,4 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: 'AIzaSyA-jK4-XxaqnjXp8TXfFoNkMU0VgVS7F04',
-    authDomain: 'thinfilm-notes.firebaseapp.com',
-    databaseURL: 'https://thinfilm-notes.firebaseio.com',
-    projectId: 'thinfilm-notes',
-    storageBucket: '',
-    messagingSenderId: '5712428482'
+  
   }
 };
